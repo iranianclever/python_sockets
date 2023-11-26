0543adb99baaff84bcc6c883bde8e402aff685f7
iranianclever <iranianclever@gmail.com>
2023-11-26 18:20:35 +0330

    improve codes

--- client.py
+++ client.py
@@ -1,8 +1,11 @@
+""" Socket is a module for communication machines with him and exchange data together. """
 import socket
 
+# A socket object to receive data from server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((socket.gethostname(), 1234))
 
-
-msg = s.recv(1024)
-print(msg.decode('utf-8'))
+# While loop for get parted data from server
+while True:
+    msg = s.recv(8)  # 1024 is a capability parameter to receive data
+    print(msg.decode('utf-8'))
--- server.py
+++ server.py
@@ -1,11 +1,12 @@
+""" Socket is a module for communication machines with him and exchange data together. """
 import socket
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((socket.gethostname(), 1234))
-s.listen()
+# A socket object to listen client
+socket_object = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket_object.bind((socket.gethostname(), 1234))
+socket_object.listen()
 
 while True:
-	clientsocket, address = s.accept()
-	print(f'Connection from {address} has been established!')
-	clientsocket.send(bytes('Welcome to the server!', 'utf-8'))
-	
+    client_socket, address = socket_object.accept()
+    print(f'Connection from {address} has been established!')
+    client_socket.send(bytes('Welcome to the server!', 'utf-8'))
